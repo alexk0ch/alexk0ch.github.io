2e23d7a4a0b09eab5fbf3daa00bb9940e8874382
oleksandr.kochetov <oleksandr.kochetov@avid.com>
2015-06-30 14:59:57 +0300

    small fixes

--- alarm-clock/js/app.js
+++ alarm-clock/js/app.js
@@ -1,28 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
+  
+  var alarmTime     = { hours: 100, minutes: 100 },
+      alarmSound    = document.querySelector('audio'),
+      alarmStatus   = document.querySelector('.alarmStatus'),
+      play          = document.querySelector('.play'),
+      clockBody     = document.querySelector('.clock-body'),
+      mousewheel    = (/Firefox/i.test(navigator.userAgent)) ? 'DOMMouseScroll' : 'mousewheel',
+      PLAY          = "&#9658;",
+      PAUSE         = "&#9726;";
 
-	var alarmTime = {
-		hours: 100, minutes: 100
-	};
-
-	var alarmSound = document.querySelector('audio');
-	var alarmStatus = document.querySelector('.alarmStatus');
-	var play = document.querySelector('.play');
-  var clockBody = document.querySelector('.clock-body');
-  var mousewheel = (/Firefox/i.test(navigator.userAgent)) ? 'DOMMouseScroll' : 'mousewheel';
-
-  var PLAY = "&#9658;";
-  var PAUSE = "&#9726;";
-
-	play.addEventListener('click', function () {
-		if (alarmSound.paused) {
-			alarmSound.play();
-			this.innerHTML = PAUSE;
-		} else {
-			this.innerHTML = PLAY;
-			alarmSound.pause();
-			alarmSound.currentTime = 0;
-		}
-	});
+  play.addEventListener('click', function () {
+    if (alarmSound.paused) {
+      alarmSound.play();
+      this.innerHTML = PAUSE;
+    } else {
+      this.innerHTML = PLAY;
+      alarmSound.pause();
+      alarmSound.currentTime = 0;
+    }
+  });
 
   alarmSound.addEventListener('play', function () {
     clockBody.classList.add('ringing');
@@ -45,113 +41,116 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 
-	function TimeTracker (parentSelector, max) {
-		this.digitLeft = document.querySelector(parentSelector + ' .time-one');
-		this.digitRight = document.querySelector(parentSelector + ' .time-two');
-		this.currentLeft = 0;
-		this.currentRight = 0;
-		this.max = max;
-		this.bindEvents(parentSelector);
-	};
+  function TimeTracker (parentSelector, max) {
+    this.digitLeft = document.querySelector(parentSelector + ' .time-one');
+    this.digitRight = document.querySelector(parentSelector + ' .time-two');
+    this.currentLeft = 0;
+    this.currentRight = 0;
+    this.max = max;
+    this.bindEvents(parentSelector);
+  };
 
-	TimeTracker.prototype.get = function () {
-		return parseInt(this.currentLeft + '' + this.currentRight);
-	};
+  TimeTracker.prototype.get = function () {
+    return parseInt(this.currentLeft + '' + this.currentRight);
+  };
 
-	TimeTracker.prototype.set = function (t) {
-		this.currentLeft = Math.floor(t / 10);
-		this.currentRight = t % 10;
-		this.render(this.currentLeft, this.currentRight);
-		setAlarmTime();
-	};
+  TimeTracker.prototype.set = function (t) {
+    this.currentLeft = Math.floor(t / 10);
+    this.currentRight = t % 10;
+    this.render(this.currentLeft, this.currentRight);
+    setAlarmTime();
+  };
 
-	TimeTracker.prototype.render = function (leftDigit, rightDigit) {
-		leftDigit === 1 
-			? this.digitLeft.setAttribute('one', '')
-			: this.digitLeft.removeAttribute('one');
+  TimeTracker.prototype.render = function (leftDigit, rightDigit) {
+    leftDigit === 1 
+      ? this.digitLeft.setAttribute('one', '')
+      : this.digitLeft.removeAttribute('one');
 
-		rightDigit === 1 
-			? this.digitRight.setAttribute('one', '')
-			: this.digitRight.removeAttribute('one');
-		
-		this.digitLeft.textContent =  leftDigit;
-		this.digitRight.textContent =  rightDigit;
-	};
+    rightDigit === 1 
+      ? this.digitRight.setAttribute('one', '')
+      : this.digitRight.removeAttribute('one');
+    
+    this.digitLeft.textContent =  leftDigit;
+    this.digitRight.textContent =  rightDigit;
+  };
 
-	TimeTracker.prototype._increase = function () {
-		var current = this.get();
-		(current + 1 <= this.max) ? this.set(++current) : this.set(0);
-	};
+  TimeTracker.prototype._increase = function () {
+    var current = this.get();
+    (current + 1 <= this.max) ? this.set(++current) : this.set(0);
+  };
 
-	TimeTracker.prototype._decrease = function () {
-		var current = this.get();
-		(current - 1 >= 0) ? this.set(--current) : this.set(this.max);
-	};
+  TimeTracker.prototype._decrease = function () {
+    var current = this.get();
+    (current - 1 >= 0) ? this.set(--current) : this.set(this.max);
+  };
 
-	TimeTracker.prototype.bindEvents = function (parentSelector) {
-		var m = document.querySelector(parentSelector + ' .more');
-		var l = document.querySelector(parentSelector + ' .less');
+  TimeTracker.prototype.bindEvents = function (parentSelector) {
+    var more = document.querySelector(parentSelector + ' .more'),
+        less = document.querySelector(parentSelector + ' .less');
 
-		m.addEventListener('click', this._increase.bind(this));		
-		l.addEventListener('click', this._decrease.bind(this));
+    more.addEventListener('click', this._increase.bind(this));    
+    less.addEventListener('click', this._decrease.bind(this));
 
-		var mousedown = null;
-		var interval = 200;
-		var _this = this;
+    var mousedown = null,
+        interval = 200,
+        _this = this;
 
-		m.addEventListener('mousedown', function () {
-			mousedown = setInterval(_this._increase.bind(_this), interval);
-		});
+    more.addEventListener('mousedown', function () {
+      mousedown = setInterval(_this._increase.bind(_this), interval);
+    });
 
-		m.addEventListener('mouseup', function () {
-			clearInterval(mousedown)
-		});
+    more.addEventListener('mouseup', function () {
+      clearInterval(mousedown)
+    });
 
-		l.addEventListener('mousedown', function () {
-			mousedown = setInterval(_this._decrease.bind(_this), interval);
-		});
+    less.addEventListener('mousedown', function () {
+      mousedown = setInterval(_this._decrease.bind(_this), interval);
+    });
 
-		l.addEventListener('mouseup', function () {
-			clearInterval(mousedown)
-		});
-	};
+    less.addEventListener('mouseup', function () {
+      clearInterval(mousedown)
+    });
+  };
 
-	var hours = new TimeTracker ('.hours', 23);
-	var minutes = new TimeTracker ('.minutes', 59);
-	var currentTime = new CurrentTime ('.currentTime');
+  var hours = new TimeTracker ('.hours', 23),
+      minutes = new TimeTracker ('.minutes', 59),
+      currentTime = new CurrentTime ('.currentTime');
 
-	function CurrentTime (selector) {
-		var all = document.querySelectorAll(selector + ' span'),
-			h = all[0], m = all[1], s = all[2];
+  function CurrentTime (selector) {
+    var all   = document.querySelectorAll(selector + ' span'),
+          h   = all[0], 
+          m   = all[1], 
+          s   = all[2];
 
-		setInterval(function () {
-			var d = new Date();
-			var _h = d.getHours();
-			var _m = d.getMinutes();
-			var _s = d.getSeconds();
-			checkAlarm(_h, _m);
+    setInterval(function () {
+      var d   = new Date(),
+          _h  = d.getHours(),
+          _m  = d.getMinutes(),
+          _s  = d.getSeconds();
 
-			h.textContent = prefixZero(_h);
-			m.textContent = prefixZero(_m);
-			s.textContent = prefixZero(_s);
-		}, 1000);
-	};
+      checkAlarm(_h, _m);
 
-	function prefixZero (n) {
-		return n < 10 ? 0 + "" + n : n;
-	};
+      h.textContent = prefixZero(_h);
+      m.textContent = prefixZero(_m);
+      s.textContent = prefixZero(_s);
+    }, 1000);
+  };
 
-	function setAlarmTime () {
-		alarmTime = {
-			hours: hours.get(),
-			minutes: minutes.get()
-		};		  
-		alarmStatus.textContent = '✔OK';
-	};
+  function prefixZero (n) {
+    return n < 10 ? 0 + "" + n : n;
+  };
 
-	function checkAlarm (h, m) {
-		if (h != alarmTime.hours && m != alarmTime.minutes) return;
+  function setAlarmTime () {
+    alarmTime = {
+      hours: hours.get(),
+      minutes: minutes.get()
+    };      
+    alarmStatus.textContent = '✔OK';
+  };
+
+  function checkAlarm (h, m) {
+    if (h != alarmTime.hours || m != alarmTime.minutes) return;
     if (alarmSound.paused) alarmSound.play();
-	};
+  };
 
 });
